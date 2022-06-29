256fc91535758b3e2d9cf66204b208246330dd81
janutharinda <108401294+janutharinda@users.noreply.github.com>
2022-06-29 16:16:57 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -23,27 +23,27 @@ global.APIKeys = {
 global.alivemsg = `My name is RED DRAGON🔥
 My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+94788230710'] //Owner number in aive msg
+global.premium = ['+94788230710'] //Owner Number info
+global.ownernomer = '+94788230710' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
-global.botname = ' ☬RED༒DRAGON' //Bot Name
+global.ownername = 'Jᵃⁿᵘ ʙᴏᴛ @₂₀₂₂🐱♥️' //Owner Name
+global.botname = ' *ʲᵃⁿᵘ ʙᴏᴛ_🐱♥️* ' //Bot Name
 
 global.button = '🤭මේක ටච් කරන්න📍' //Costomize A Button Name In Alive Message
 global.btnurl = '🙂🔥දැන් සැපද?😑' //Change Your Button's URL
 
 global.region = 'India, South India, Kerala' //Country Region
 
-global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
-global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
+global.packname = 'Jᵃⁿᵘ ʙᴏᴛ @₂₀₂₂🐱♥️' //Bot Pacage Name
+global.author = '*ʲᵃⁿᵘ ʙᴏᴛ_🐱♥️* ' //Author Name
 global.prefa = ['','!','.','😂','❤','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor In Menu!
 
 
-global.ig = 'https://github.com/prabhasha2006' //Github Account!
-global.footer = '©_Kumuthu.' // DO NOT CHANGE!
+global.ig = 'http://Wa.me/+94760252728?text=_හායි.....🕊️♥️_බ්‍රොක්කාස්ට්_වලට_Add_නොකිරීමට_පොරොන්දු_වෙමි_🙃🌸' //Github Account!
+global.footer = 'Jᵃⁿᵘ ʙᴏᴛ @₂₀₂₂🐱♥️' // DO NOT CHANGE!
 
 global.mess = { // Error Mesages
     success: 'Done ✅',
@@ -53,7 +53,7 @@ global.mess = { // Error Mesages
     group: 'Feature Used Only For Groups!',
     private: 'This Features Only For Private Chat!',
     bot: 'This Feature Is Only For Bot😝',
-    wait: '👉👈Pleas Wait...',
+    wait: 'මදක් රැදි සිටින්න....👉👈',
     error: 'Error!!! Maybe Api Key Is Expired🤔!',
     endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
 }
